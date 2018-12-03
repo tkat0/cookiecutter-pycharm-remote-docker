0c3b560e4165e1cff8925827d5b9cda6579f23da
tkat0 <tkato.dev@gmail.com>
2018-12-04 01:15:12 +0900

    update

---
+++ {{cookiecutter.project_name}}/main.py
@@ -0,0 +1,6 @@
+def main():
+    print('hello {{ cookiecutter.project_name }}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ {{cookiecutter.project_name}}/tests/test_example.py
@@ -0,0 +1,5 @@
+def test_example():
+    # This is dummy unittest(pytest). Please delete it.
+    a = 1
+    b = 1
+    assert a + b == 2
